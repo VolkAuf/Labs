1c0683db4d0669d1cfc758bc6316967a97eab3b4
RAFMAN <volkov.rafman@mail.ru>
2021-10-19 13:42:22 +0400

    Lab1_Fix

--- lab1.py
+++ lab1.py
@@ -3,13 +3,13 @@ import pandas as pd
 import random
 import cowsay as sas
 
-table_size = 1000
+table_size = 500
 
 
 def generate_noise(noise, arr):
     for i in range(random.randint(0, table_size)):
         be_or_not_be = random.randint(0, 10)
-        if be_or_not_be > 7:
+        if be_or_not_be > 9:
             line = random.randint(0, table_size - 1)
             if arr[line] != np.nan:
                 head = int(arr[line])
@@ -18,7 +18,7 @@ def generate_noise(noise, arr):
 
 
 def float_to_int(arr):
-    for i in range(1000):
+    for i in range(table_size):
         arr[i] = round(arr[i], 0)
         arr[i] = int(arr[i])
 
@@ -26,31 +26,37 @@ def float_to_int(arr):
 def generate_nan(arr):
     for i in range(random.randint(0, table_size)):
         be_or_not_be = random.randint(0, 10)
-        if be_or_not_be > 8:
+        if be_or_not_be > 9:
             line = random.randint(0, table_size - 1)
             arr[line] = np.nan
 
 
 def start_lab1():
-    akkum = np.random.normal(4000, 2000, table_size)
+    akkum = np.random.normal(4000, 1000, table_size)
     float_to_int(akkum)
     generate_noise(10000, akkum)
     generate_nan(akkum)
 
-    cam_pix = np.random.normal(8, 4, table_size)
+    cam_pix = np.random.normal(8, 2, table_size)
     float_to_int(cam_pix)
     generate_noise(130, cam_pix)
     generate_nan(cam_pix)
 
-    ram = np.random.randint(2000, 6000, table_size) * 0.1
+    rom = np.random.randint(32, 128,  table_size) * 0.1 * 10
+    float_to_int(rom)
+    generate_noise(524, rom)
+    generate_nan(rom)
+
+    ram = np.random.exponential(2, table_size) + 1
     float_to_int(ram)
     generate_noise(64, ram)
     generate_nan(ram)
 
-    rom = np.random.exponential(4000, table_size)
-    float_to_int(rom)
-    generate_noise(1024, rom)
-    generate_nan(rom)
+
+
+    # my_table = {'Аккумулятор ': akkum}
+    # , 'Пиксели камеры ': cam_pix, 'ПЗУ ': rom, 'ОЗУ ': ram
+    # =СЧЁТЕСЛИ(A2:A501;(">6000"))+СЧЁТЕСЛИ(A2:A501;("<2000"))
 
     my_table = {'Аккумулятор ': akkum, 'Пиксели камеры ': cam_pix, 'ПЗУ ': rom, 'ОЗУ ': ram}
 
